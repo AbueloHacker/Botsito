454eee67741a79aa1f81ac52b8e84d0e2d14d9a3
AbueloHacker <74150812+AbueloHacker@users.noreply.github.com>
2024-01-08 08:56:10 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5491168352204']
+  ['51976472278', 'LORD', true],
+  ['51917734094']
 ] //Numeros de owner 
 
 global.mods = [''] 
-global.prems = ['50489079501', '573143917092']
+global.prems = ['51917734094', '51976472278']
 global.APIs = { // API Prefix
   // name: 'https://website' 
   nrtm: 'https://fg-nrtm.ddns.net',
